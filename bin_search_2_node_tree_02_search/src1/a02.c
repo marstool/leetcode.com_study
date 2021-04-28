5b4497cb94d5782adc11cdd95520f416bf8bbca6
dyn <y@e>
2021-04-28 09:14:13 -0700

    20210428_0914AM_

--- bin_search_2_node_tree_02_search/src1/a02.c
+++ bin_search_2_node_tree_02_search/src1/a02.c
@@ -1,27 +1,67 @@
 #include "h00.h"
 
 
+#define _max_treeAmount    32    // moust be 2 ^^ 1,2,3,4,5.... 2, 4, 8 , 16 , 32
+TreeNode _treeNodeArr[ _max_treeAmount ] ;
+TreeNode * _treeNode_top ;
+char _str_null[] = "null" ;
+
+void _treeNodeCreateOne( char *___sbuf , int ___idx ) {
+    if ( 1 == ___idx ) { printf("\n"); } ; printf("[%d]<%s>\n", ___idx , ___sbuf);
+    /*
+       if ( __cn02 == 4 
+       && ___sbuf[0] == 'n' 
+       && ___sbuf[1] == 'u' 
+       && ___sbuf[2] == 'l' 
+       && ___sbuf[3] == 'l' 
+       ) {
+       } else {
+       }
+       */
+} // _treeNodeCreateOne
+
 TreeNode * _treeNodeCreate( char *___sbuf ) {
     int __cn01 ;
-    char *__ch01 ;
     int __cn02 ;
-    printf( "trying to create for <%s>, get < " , ___sbuf?___sbuf:"(null)" );
-    if ( NULL == ___sbuf ) { return NULL ; }
+    char *__cp01 ;
+    char __buf[256] ;
 
-    for ( __cn01 = 10 ; __cn01 > 0 ; __cn01 -- ){
-        __ch01 = index(___sbuf , ',');
-        if ( NULL == __ch01 ) {
-            printf("(null)\n");
+    _treeNode_top = NULL ;
+
+    printf( "trying to create for <%s>, get < " , ___sbuf?___sbuf:"(null)" );
+    if ( NULL == ___sbuf ) {  // EOF met
+        return NULL ; 
+    }
+
+    for ( __cn01 = 1 ; __cn01 < _max_treeAmount ; __cn01 ++ ){
+        __cp01 = index(___sbuf , ',');
+        if ( NULL == __cp01 ) {
+            __cn02 = 0 ;
         } else {
-            __cn02 = __ch01 - ___sbuf ;
-            printf("(%d, %d)\n", __ch01, __cn02 );
+            __cn02 = __cp01 - ___sbuf ;
         }
 
-        if ( NULL == __ch01 ) { break ; }
+        strncpy( __buf , ___sbuf , __cn02 );
+        __buf[__cn02] = 0 ;
+        _treeNodeCreateOne( __buf , __cn01 ) ;
+
+        if ( 1 == __cn01 ) {
+            _treeNode_top = _treeNodeArr ;
+        }
+
+        if ( NULL == __cp01 ) {
+            __cn01 ++ ;
+            while ( __cn01 < _max_treeAmount ) { // set NULL to the remain
+                _treeNodeCreateOne( _str_null , __cn01 ) ;
+                __cn01 ++ ;
+            }
+            break ;
+        }
+
         ___sbuf += __cn02 + 1 ;
     }
 
-    return NULL ;
+    return _treeNode_top ;
 }
 int               _treeNodeFree( TreeNode * ___treeNodeTop){
     return 0 ;
