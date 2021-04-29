bd6f96f80c660d1dedef9328965f2b2025a03966
Your Name <you@example.com>
2021-04-28 22:27:29 -0700

    20210428_2227PM_

--- bin_search_2_node_tree_02_search/src1/a04.c
+++ bin_search_2_node_tree_02_search/src1/a04.c
@@ -9,12 +9,12 @@ void _dumpTreeNode(TreeNode* root){
     for ( __i01 = 0 ; __i01 < _max_treeAmount ; __i01 ++ ) {
         __tp = root + __i01 ;
 
-        if (NULL == __tp -> left) printf( "L(n)" );
+        if (NULL == __tp -> left) printf( "L(*)" );
         else                      printf( "L(%d)", __tp -> left -> val );
 
         printf( " %d ", __tp -> val );
 
-        if (NULL == __tp -> right) printf( "R(n)" );
+        if (NULL == __tp -> right) printf( "R(*)" );
         else                       printf( "R(%d)", __tp -> left -> val );
         
         if ( 3 == __i01 % 4 ) {
