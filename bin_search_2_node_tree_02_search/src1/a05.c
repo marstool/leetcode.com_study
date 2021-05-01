7b61b442a6597d155f1ed4f2f5c4c32bb1678f09
Your Name <you@example.com>
2021-05-01 13:23:06 -0700

    20210501_1323PM_

---
+++ bin_search_2_node_tree_02_search/src1/a05.c
@@ -0,0 +1,27 @@
+#include "h00.h"
+
+BSTIterator* bSTIteratorCreate(struct TreeNode* root) {
+    _ST01 . nodeRoot = root ;
+    _ST01 . curNode  = _findPrev( root ) ;
+    return &_ST01 ;
+}
+
+int bSTIteratorNext(BSTIterator* obj) {
+    int __rt ; 
+    if ( false == bSTIteratorHasNext( obj ) ) return -1 ;
+    __rt = obj -> curNode -> val ; 
+    obj -> curNode = _findNext( obj -> nodeRoot , obj -> curNode ) ;
+    return __rt ;
+}
+
+bool bSTIteratorHasNext(BSTIterator* obj) {
+    if ( NULL == obj  ) return false ;
+    if ( NULL == obj -> curNode ) return false ;
+    return true ;
+}
+
+
+void bSTIteratorFree(BSTIterator* obj) {
+    
+}
+
