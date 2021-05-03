20148683b713892fb3615da5ed17d0f041c475f0
Your Name <you@example.com>
2021-05-03 09:14:46 -0700

    20210503_0914AM_

--- bin_search_2_node_tree_02_search/src1/a05.c
+++ bin_search_2_node_tree_02_search/src1/a05.c
@@ -40,19 +40,50 @@ void bSTIteratorFree(BSTIterator* ___iter) {
 }
 
 struct TreeNode* deleteNode(struct TreeNode* ___tn, int key){
-    TreeNode* __tmpTN ;
+    TreeNode* __tmpTN  ;
+    TreeNode* __tmpTN2 ;
+    //bool __replace_root ;
+
     if ( NULL == ___tn ) return NULL ;
 
     printf( "1838181 : trying delete %d\n" , key ) ;
 
-    if ( key == ___tn -> val ) {
-        if ( NULL == ___tn -> right ) {
+    if ( key == ___tn -> val ) { // try delete the root
+        if ( NULL == ___tn -> left ) { // delete the root while no left-child
+            __tmpTN = ___tn -> right ;
+            _freeOneNode( ___tn ) ;
+            return __tmpTN ;
+        }
+        if ( NULL == ___tn -> right ) { // delete the root while no right-child
             __tmpTN = ___tn -> left ;
             _freeOneNode( ___tn ) ;
             return __tmpTN ;
         }
+
+        // both left and right exist, delete root.
+        __tmpTN = ___tn -> right ;
+        __tmpTN2 = __tmpTN -> left ;
+        if ( NULL == __tmpTN2 ){
+            __tmpTN -> left = ___tn -> left ;
+            _freeOneNode( ___tn ) ;
+            return __tmpTN ;
+        }
+
+        while ( NULL != __tmpTN2 -> left ) {
+            __tmpTN2 = __tmpTN2 -> left ;
+        }
+
+        __tmpTN2 -> left = ___tn -> left ;
+        _freeOneNode( ___tn ) ;
+        return __tmpTN ;
     }
 
-    return ___tn ;
+    if ( key > ___tn -> val ) { // try delete the root-right-child
+        deleteNode( ___tn -> right , key);
+    } else { // try delete the root-left-child
+        deleteNode( ___tn -> left  , key);
+    }
+
+    return ___tn ; // keep the origin root
 } // deleteNode
 
